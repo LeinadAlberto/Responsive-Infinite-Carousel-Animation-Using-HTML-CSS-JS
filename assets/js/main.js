753af817dc861d3d03b090ef7022fcf9aa1d46a5
Alberto <dany.canaviri2022@gmail.com>
2025-10-26 23:50:35 -0400

    Infinite carousel JavaScript and animate infinite carousel, completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 /*=============== DUPLICATE CARD CAROUSEL ===============*/
+// Duplicate images to make the animation work
+const tracks = document.querySelectorAll('.carousel__content')
+
+tracks.forEach(track => {
+    const cards = [...track.children]
+    console.log(cards)
+
+    // Duplicate cards only once
+    for (const card of cards) {
+        track.appendChild(card.cloneNode(true))
+    }
+})
